b6ae2c2533aa94fb4f1b17b788c24e85048f1d65
mschiegg <martin.schiegg@iwr.uni-heidelberg.de>
2014-10-31 16:03:46 +0100

    experimental: playground for gpc

--- lazyflow/classifiers/gaussianProcessClassifier.py
+++ lazyflow/classifiers/gaussianProcessClassifier.py
@@ -4,14 +4,27 @@ import cPickle as pickle
 from GPy import kern
 import numpy
 import h5py
+import copy
 
 import GPy
 from .lazyflowClassifier import LazyflowVectorwiseClassifierABC, LazyflowVectorwiseClassifierFactoryABC
 
 import logging
+import numpy as np
+
 logger = logging.getLogger(__name__)
 
 class GaussianProcessClassifierFactory(LazyflowVectorwiseClassifierFactoryABC):
+    parameters = { "max_iters_hyperparameters": 50,
+                   "max_iters_ep": 5,
+                   "max_iters_initializations": 1,
+                   "num_inducing": 20,
+                   "kernel": 'rbf',
+                   "normalize_X": True,
+                   "normalize_Y": False,
+                   # "lengthscales": [200, 0.5,1,50]
+                   }
+
     def __init__(self, *args, **kwargs):
         self._args = args
         self._kwargs = kwargs
@@ -24,27 +37,69 @@ class GaussianProcessClassifierFactory(LazyflowVectorwiseClassifierFactoryABC):
         
         X = numpy.asarray(X, numpy.float32)
         y = numpy.asarray(y, numpy.uint32)
+
+        print np.sum(np.std(X,axis=0)==0), 'of', X.shape[1], 'features are constant'
+
         if y.ndim == 1:
             y = y[:, numpy.newaxis]
-        
-        y-=1 #TODO: Don't do this.
+
+        y-=1 # FIXME: for the binary case, we need classes 0 and 1 rather than 1 and 2. Fix for the multi-label case
         assert X.ndim == 2
         assert len(X) == len(y)
         assert all(i in (0,1) for i in numpy.unique(y))
-        
-        # get control of default parameters
-        if not "kernel" in self._kwargs:
-            self._kwargs["kernel"]=kern.rbf(X.shape[1])
-        if not "num_inducing" in self._kwargs:
-            self._kwargs["num_inducing"]=50
+
+        if self.parameters['kernel'] == 'rbf':
+            self._kwargs["kernel"] = kern.rbf(X.shape[1])
+        elif self.parameters['kernel'] == 'rbf_ard':
+            self._kwargs["kernel"] = kern.rbf(X.shape[1], ARD=True)
+        else:
+            raise NotImplementedError, 'this kernel is not supported yet'
+
+        self._kwargs["normalize_X"] = bool(self.parameters['normalize_X'])
+        self._kwargs["normalize_Y"] = bool(self.parameters['normalize_Y'])
+        self._kwargs["num_inducing"] = self.parameters['num_inducing']
+
         classifier = GPy.models.SparseGPClassification(X,
                                                        y,
                                                         **self._kwargs)
+
+        # constrain all lengthscale parameters to be positive
         classifier.tie_params('.*len')
-        #print classifier
+        # classifier.update_likelihood_approximation()
+        # classifier.ensure_default_constraints()
+
+        best = (None, None, None)
+
+        xDim = np.sqrt(X.shape[1])
+        lengthscales = [ xDim, xDim / 2., xDim / 4., xDim * 2, xDim * 4 ]
+
+        for lengthscale in lengthscales:
+            # for j in range(self.parameters['max_iters_initializations']):
+            #     print 'randomize initialization'
+            #     classifier.randomize()
+            classifier.kern = kern.rbf(X.shape[1], lengthscale=lengthscale)
+            classifier.update_likelihood_approximation()
+
+            for i in range(self.parameters['max_iters_ep']):
+                print 'ep iteration'
+                classifier.optimize(max_iters=self.parameters['max_iters_hyperparameters'])
+
+            print 'log likelihood:', classifier.log_likelihood(), 'lengthscale:', lengthscale
+            if best[1] is None or classifier.log_likelihood() > best[1]:
+                print 'that is better, setting best to', lengthscale
+                best = (lengthscale, classifier.log_likelihood(), copy.copy(classifier.getstate()))
+        print '-----'
+
+        # classifier.kern = kern.rbf(X.shape[1], lengthscale=best[0])
+        classifier.setstate(best[2])
+
         classifier.update_likelihood_approximation()
-        classifier.ensure_default_constraints()
-        classifier.optimize(max_iters=100)
+        # classifier.optimize(max_iters=1)
+        for i in range(self.parameters['max_iters_ep']):
+            classifier.optimize(max_iters=self.parameters['max_iters_hyperparameters'])
+
+        print 'log likelihood:', classifier.log_likelihood(), 'lengthscale:', best[0]
+
         return GaussianProcessClassifier( classifier, known_labels )
 
     @property
